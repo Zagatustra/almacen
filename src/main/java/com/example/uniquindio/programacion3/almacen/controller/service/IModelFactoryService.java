dbf48f1f47c6034cfb049ea5747991d1f73334af
Jordy Diaz <diaz.jordyb@gmail.com>
2024-02-19 01:03:22 -0500

    chages

--- src/main/java/com/example/uniquindio/programacion3/almacen/controller/service/IModelFactoryService.java
+++ src/main/java/com/example/uniquindio/programacion3/almacen/controller/service/IModelFactoryService.java
@@ -10,15 +10,5 @@ public interface IModelFactoryService {
     boolean actualizarCliente(ClienteDto clienteDto);
 
     boolean eliminarCliente(ClienteDto clienteDto);
-    import com.example.uniquindio.programacion3.almacen.mapping.dto.ClienteDto;
 
-    public interface IModelFactoryService {
-
-
-        boolean actualizarCliente(ClienteDto clienteDto);
-
-        //
-        boolean eliminarCliente(ClienteDto clienteDto);
-
-    }
 }
--- target/generated-sources/annotations/com/example/uniquindio/programacion3/almacen/mapping/mapper/MapperAlmacenImpl.java
+++ target/generated-sources/annotations/com/example/uniquindio/programacion3/almacen/mapping/mapper/MapperAlmacenImpl.java
@@ -2,16 +2,76 @@ package com.example.uniquindio.programacion3.almacen.mapping.mapper;
 
 import com.example.uniquindio.programacion3.almacen.mapping.dto.ClienteDto;
 import com.example.uniquindio.programacion3.almacen.mapping.dto.ProductoDto;
-import com.example.uniquindio.programacion3.almacen.model.Cliente;
-import com.example.uniquindio.programacion3.almacen.model.Producto;
 import com.example.uniquindio.programacion3.almacen.model.Juridico;
 import com.example.uniquindio.programacion3.almacen.model.Natural;
+import com.example.uniquindio.programacion3.almacen.model.Producto;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-02-19T01:03:01-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+)
+*/
 public class MapperAlmacenImpl implements MapperAlmacen {
+
+    @Override
+    public ProductoDto productoToProductoDto(Producto producto) {
+        if ( producto == null ) {
+            return null;
+        }
+
+        String codigo = null;
+        String nombreProducto = null;
+        String descripcion = null;
+        double valor = 0.0d;
+        int cantidad = 0;
+
+        codigo = producto.getCodigo();
+        nombreProducto = producto.getNombreProducto();
+        descripcion = producto.getDescripcion();
+        valor = producto.getValor();
+        cantidad = producto.getCantidad();
+
+        ProductoDto productoDto = new ProductoDto( codigo, nombreProducto, descripcion, valor, cantidad );
+
+        return productoDto;
+    }
+
+    @Override
+    public Producto productoDtoToProducto(ProductoDto productoDto) {
+        if ( productoDto == null ) {
+            return null;
+        }
+
+        Producto producto = new Producto();
+
+        producto.setCodigo( productoDto.codigo() );
+        producto.setNombreProducto( productoDto.nombreProducto() );
+        producto.setDescripcion( productoDto.descripcion() );
+        producto.setValor( productoDto.valor() );
+        producto.setCantidad( productoDto.cantidad() );
+
+        return producto;
+    }
+
+    @Override
+    public List<ProductoDto> getProductosDto(List<Producto> listaProductos) {
+        if ( listaProductos == null ) {
+            return null;
+        }
+
+        List<ProductoDto> list = new ArrayList<ProductoDto>( listaProductos.size() );
+        for ( Producto producto : listaProductos ) {
+            list.add( productoToProductoDto( producto ) );
+        }
+
+        return list;
+    }
 
     @Override
     public ClienteDto naturalToClienteDto(Natural cliente) {
@@ -130,60 +190,6 @@ public class MapperAlmacenImpl implements MapperAlmacen {
         List<ClienteDto> list = new ArrayList<ClienteDto>( listaJur.size() );
         for ( Juridico juridico : listaJur ) {
             list.add( juridicoToClienteDto( juridico ) );
-        }
-
-        return list;
-    }
-
-    @Override
-    public ProductoDto productoToProductoDto(Producto producto) {
-        if ( producto == null ) {
-            return null;
-        }
-
-        String codigo = null;
-        String nombreProducto = null;
-        String descripcion = null;
-        double valor = 0.0d;
-        int cantidad = 0;
-
-        codigo = producto.getCodigo();
-        nombreProducto = producto.getNombreProducto();
-        descripcion = producto.getDescripcion();
-        valor = producto.getValor();
-        cantidad = producto.getCantidad();
-
-        ProductoDto productoDto = new ProductoDto( codigo, nombreProducto, descripcion, valor, cantidad );
-
-        return productoDto;
-    }
-
-    @Override
-    public Producto productoDtoToProducto(ProductoDto productoDto) {
-        if ( productoDto == null ) {
-            return null;
-        }
-
-        Producto producto = new Producto();
-
-        producto.setCodigo( productoDto.codigo() );
-        producto.setNombreProducto( productoDto.nombreProducto() );
-        producto.setDescripcion( productoDto.descripcion() );
-        producto.setValor( productoDto.valor() );
-        producto.setCantidad( productoDto.cantidad() );
-
-        return producto;
-    }
-
-    @Override
-    public List<ProductoDto> getProductosDto(List<Producto> listaProductos) {
-        if ( listaProductos == null ) {
-            return null;
-        }
-
-        List<ProductoDto> list = new ArrayList<ProductoDto>( listaProductos.size() );
-        for ( Producto producto : listaProductos ) {
-            list.add( productoToProductoDto( producto ) );
         }
 
         return list;
